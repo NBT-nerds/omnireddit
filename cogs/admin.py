a6e6610a3c8e3855efe4d2a9dad27b3891691465
Patrick Jones <meonint@outlook.com>
2020-09-14 20:43:52 +0100

    added suggest command and reloads

---
+++ cogs/admin.py
@@ -0,0 +1,33 @@
+from discord.ext import commands
+import discord
+import os
+from utils.util import bot_staff, getConfig
+from loguru import logger
+    
+class Admin(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot: commands.Bot = bot
+
+
+    @commands.check(bot_staff)
+    @commands.command()
+    async def reload(self, ctx : commands.Context):
+        for filename in os.listdir("./cogs"):
+            if filename.endswith(".py"):
+                try:
+                    self.bot.reload_extension(f"cogs.{filename[:-3]}")
+                    logger.info(f"reloaded cogs.{filename[:-3]}")
+                except commands.ExtensionNotLoaded:
+                    self.bot.load_extension(f"cogs.{filename[:-3]}")
+                    logger.info(f"loaded cogs.{filename[:-3]}")
+        await ctx.message.add_reaction("👌")
+
+    @commands.command()
+    @commands.check(bot_staff)
+    async def reload_config(self, ctx):
+        self.bot.config = getConfig()
+        return await ctx.send(f"`config reloaded by` {ctx.author.mention}")
+
+def setup(bot):
+    bot.add_cog(Admin(bot))
---
+++ cogs/misc.py
@@ -0,0 +1,21 @@
+from discord.ext import commands
+import discord
+from utils.util import Embed
+
+
+class Misc(commands.Cog):
+
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.command()
+    async def suggest(self, ctx, *, suggestion: str):
+        embed = Embed(ctx, description=suggestion).author().random_footer()
+        msg = await self.bot.get_guild(self.bot.config["support_guild"]["id"]).get_channel(self.bot.config["support_guild"]["suggestion_channel"]).send(embed=embed)
+        await msg.add_reaction("✅")
+        await msg.add_reaction("❎")
+        await ctx.send("Suggestion logged!")
+
+
+def setup(bot):
+    bot.add_cog(Misc(bot))
