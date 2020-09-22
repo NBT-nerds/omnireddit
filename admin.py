d5646a0673f39ae0411fe3a22b2b8c129319f832
Amethysm <59629187+Amethysm@users.noreply.github.com>
2020-09-22 23:43:37 +0700

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,131 @@
+import inspect
+import io
+import textwrap
+import traceback
+from contextlib import redirect_stdout
+
+from discord.ext import commands
+import discord
+import os
+from utils.util import bot_staff, getConfig
+from loguru import logger
+
+
+class Admin(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot: commands.Bot = bot
+
+    @commands.check(bot_staff)
+    @commands.command(hidden=True)
+    async def reload(self, ctx: commands.Context):
+        for filename in os.listdir("./cogs"):
+            if filename.endswith(".py"):
+                try:
+                    self.bot.reload_extension(f"cogs.{filename[:-3]}")
+                    logger.info(f"reloaded cogs.{filename[:-3]}")
+                except commands.ExtensionNotLoaded:
+                    self.bot.load_extension(f"cogs.{filename[:-3]}")
+                    logger.info(f"loaded cogs.{filename[:-3]}")
+        logger.info("Cogs reloaded, bot ready")
+        await ctx.message.add_reaction("👌")
+        
+    @commands.command(hidden=True)
+    @commands.check(bot_staff)
+    async def presence(self,ctx,*,args):
+    	await bot.change_presence(status = discord.Status.online, activity=discord.Activity(type=discord.ActivityType.playing, name=args)
+
+    @commands.command(hidden=True)
+    @commands.check(bot_staff)
+    async def reload_config(self, ctx):
+        self.bot.config = getConfig()
+        return await ctx.send(f"`config reloaded by` {ctx.author.mention}")
+
+    @commands.command(name='eval', hidden=True)
+    @commands.check(bot_staff)
+    async def _eval(self, ctx, *, body):
+        """Evaluates python code"""
+        env = {
+            'ctx': ctx,
+            'bot': self.bot,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message,
+            'source': inspect.getsource,
+            '__import__': __import__,
+            'discord': discord,
+            'commands': commands
+        }
+
+        def cleanup_code(content):
+            """Automatically removes code blocks from the code."""
+            # remove ```py\n```
+            if content.startswith('```') and content.endswith('```'):
+                return '\n'.join(content.split('\n')[1:-1])
+
+            # remove `foo`
+            return content.strip('` \n')
+
+        env.update(globals())
+
+        body = cleanup_code(body)
+        stdout = io.StringIO()
+        err = out = None
+
+        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+        def paginate(text: str):
+            '''Simple generator that paginates text.'''
+            last = 0
+            pages = []
+            for curr in range(0, len(text)):
+                if curr % 1980 == 0:
+                    pages.append(text[last:curr])
+                    last = curr
+                    appd_index = curr
+            if appd_index != len(text) - 1:
+                pages.append(text[last:curr])
+            return list(filter(lambda a: a != '', pages))
+
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            err = await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+            return await ctx.message.add_reaction('\u2049')
+
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            err = await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+        else:
+            value = stdout.getvalue()
+            if ret is None:
+                if value:
+                    try:
+
+                        out = await ctx.send(f'```py\n{value}\n```')
+                    except:
+                        paginated_text = paginate(value)
+                        for page in paginated_text:
+                            if page == paginated_text[-1]:
+                                out = await ctx.send(f'```py\n{page}\n```')
+                                break
+                            await ctx.send(f'```py\n{page}\n```')
+            else:
+                try:
+                    out = await ctx.send(f'```py\n{value}{ret}\n```')
+                except:
+                    paginated_text = paginate(f"{value}{ret}")
+                    for page in paginated_text:
+                        if page == paginated_text[-1]:
+                            out = await ctx.send(f'```py\n{page}\n```')
+                            break
+                        await ctx.send(f'```py\n{page}\n```')
+
+
+def setup(bot):
+    bot.add_cog(Admin(bot))
